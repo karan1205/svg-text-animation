1cb77d43eaec1f1141bbc9388e11e2a28545bc9b
Karan Singh <karan1205singh@gmail.com>
2017-04-28 01:45:14 +0530

    Delete script.js

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-var items = ["#pattern-rect", "#pattern-circle", "#pattern-triangle","#pattern-star"];
-$("div").hover(function(){
-  $("g").css({"visibility": "visible"});
-  var item = Math.floor(Math.random()*items.length);
-  $("g").css({"fill": "url(" + items[item] + ")"});
-  }, function(){
-  $("g").css({"visibility": "hidden"});
-  });
-$("g").css({"visibility": "hidden"});
